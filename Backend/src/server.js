273cf898b56546301332c2c2b2693bc1c41c2a30
Shubham Thakur <shubhamthakur@example.com>
2026-04-09 18:28:28 +0530

    deployment config added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,27 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hey! from server h" });
 });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "This is health end-point" });
+});
+app.get("/books", (req, res) => {
+  res.status(200).json({ message: "This is books end-point" });
+});
+
+if (ENV.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log("Server is up and running on prot NO.", ENV.PORT);
